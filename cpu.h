c41ad775bf0b4b6d6df5f726853df1a568de6a41
Dan <drs5cm@mst.edu>
2022-01-11 13:34:42 -0600

    cpu reset + mem

--- cpu.h
+++ cpu.h
@@ -1,17 +1,49 @@
 #pragma once
 
 #include <cstdint>
+using Byte = uint8_t;
+using Word = uint16_t;
+using u32 = uint32_t;
 
+namespace cpu{
+    struct MEM{
+        static constexpr u32 MAX_MEM = 1024 * 64;
+        Byte Data[MAX_MEM];
 
-struct CPU{
-// registers
-    uint8_t A;  //accumulator
-    uint8_t X;  //index 
-    uint8_t Y;  //index
-    uint8_t Status;
-    uint8_t SP; // stack pointer
-    uint16_t PC; //program counter
+        void INIT(){
+            for (u32 i = 0; i < MAX_MEM; i++){
+                Data[i] = 0;
+            }
+        }
+    };
+    //6502
+    struct CPU{
+    // registers
+    Byte A;  //accumulator
+    Byte X;  //index 
+    Byte Y;  //index
+    Byte Status;
+    Word SP; // stack pointer
+    Word PC; //program counter
 
+    Byte C : 1; // CARRY status 
+    Byte Z : 1; // ZERO  status 
+    Byte I : 1; // INTERRUPT status 
+    Byte D : 1; // Decimal status 
+    Byte B : 1; // BREAK status 
 
+    Byte V : 1; // OVERFLOW status 
+    Byte N : 1; // NEGATIVE status 
 
-};
+        void RESET( cpu::MEM &memory){
+            PC = 0xFFFC;
+            D = 0;
+            SP = 0x00FF;
+
+            A = X = Y = C = Z
+            = I = D = B = V = N = 0;
+            memory.INIT();
+        }
+        
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "cpu.h"
+
 
 int main (int argc, char* argv[]){
+    cpu::CPU cpu6502;
+    cpu::MEM memory;
+    cpu6502.RESET( memory );
     return 0;
 }

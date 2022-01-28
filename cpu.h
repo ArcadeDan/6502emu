11d05e09f13a72b110cf3c2acee662aaa556f03c
Dan <drs5cm@mst.edu>
2022-01-28 16:32:02 -0600

    moved

--- cpu.h
+++ cpu.h
@@ -9,57 +9,6 @@ namespace e6502{
     using Word = uint16_t;
     using u32 = uint32_t;
 
-
-
-
-    struct MEM{
-        static constexpr u32 MAX_MEM = 1024 * 64;
-        Byte Data[MAX_MEM];
-
-        void INIT(){
-            for (u32 i = 0; i < MAX_MEM; i++){
-                Data[i] = 0;
-            }
-        }
-        //read byte
-        inline Byte operator[](u32 Address) const {
-            return Data[Address];
-        }
-        inline Byte &operator[](u32 Address){
-            return Data[Address];
-        }
-    };
-
-    struct ProcessorStatus{
-
-
-        Byte V : 1; // OVERFLOW status 
-        Byte N : 1; // NEGATIVE status 
-
-        Byte C : 1; // CARRY status 
-        Byte Z : 1; // ZERO  status 
-        Byte I : 1; // INTERRUPT status 
-        Byte D : 1; // Decimal status 
-        Byte B : 1; // BREAK status 
-
-    };
-
-
-    //6502
-    struct CPU{
-    
-        Byte A;     //accumulator
-        Byte X;     //index 
-        Byte Y;     //index
-        
-        Word SP;    // stack pointer
-        Word PC;    //program counter
-
-        union {
-            Byte Status;
-            ProcessorStatus Flag;
-        };
-
     /*OP CODES */      // http://www.6502.org/tutorials/6502opcodes.html
         
         /* ADC - Add with Carry */     // FLAGS : N V Z C
@@ -269,6 +218,57 @@ namespace e6502{
 
 
 
+    struct MEM{
+        static constexpr u32 MAX_MEM = 1024 * 64;
+        Byte Data[MAX_MEM];
+
+        void INIT(){
+            for (u32 i = 0; i < MAX_MEM; i++){
+                Data[i] = 0;
+            }
+        }
+        //read byte
+        inline Byte operator[](u32 Address) const {
+            return Data[Address];
+        }
+        inline Byte &operator[](u32 Address){
+            return Data[Address];
+        }
+    };
+
+    struct ProcessorStatus{
+
+
+        Byte V : 1; // OVERFLOW status 
+        Byte N : 1; // NEGATIVE status 
+
+        Byte C : 1; // CARRY status 
+        Byte Z : 1; // ZERO  status 
+        Byte I : 1; // INTERRUPT status 
+        Byte D : 1; // Decimal status 
+        Byte B : 1; // BREAK status 
+
+    };
+
+
+    //6502
+    struct CPU{
+    
+        Byte A;     //accumulator
+        Byte X;     //index 
+        Byte Y;     //index
+        
+        Word SP;    // stack pointer
+        Word PC;    //program counter
+
+        union {
+            Byte Status;
+            ProcessorStatus Flag;
+        };
+
+    
+
+
         inline void RESET( e6502::MEM &memory){
             this->PC = 0xFFFC;
             this->Flag.D = 0;
@@ -307,6 +307,7 @@ namespace e6502{
         inline void EXECUTE(u32 cycles, e6502::MEM &memory ){
             while (cycles > 0) {
                 Byte instruction = FETCH(cycles, memory);
+                /*
                 switch(instruction){
                     default:
                         std::cout << "Instruction not handled " <<
@@ -319,6 +320,7 @@ namespace e6502{
                         this->Flag.N = (A & 0b10000000) > 0;
                         break;
                 }
+                */
             }
         }
     };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 
 
 int main (int argc, char* argv[]){
-    cpu::CPU cpu6502;
-    cpu::MEM memory;
+    e6502::CPU cpu6502;
+    e6502::MEM memory;
     cpu6502.RESET( memory );
-    memory[0xFFFC] = cpu::CPU::OP_LDA;
+    memory[0xFFFC] = e6502::OP_LDA;
     memory[0xFFFD] = 0x32;
     cpu6502.EXECUTE(2, memory);
     return 0;

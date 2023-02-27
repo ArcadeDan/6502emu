fe4468aa048847cf36d722697a1694724842124a
Dan Sparks <lordaniel24@gmail.com>
2023-02-27 14:43:27 -0600

    memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use array_macro::array;
+
+
+type Byte = u8;
+type Word = u16;
+
+struct MEMORY {
+    Data: [Byte; 1024 * 64],
+
+}
+
+impl MEMORY {
+    fn new() -> Self {
+        Self { Data: array!([u8; 1024 * 64]) }
+    }
+
+
+}
+
+
+
 fn main() {
     println!("good bye cruel world...");
 }
